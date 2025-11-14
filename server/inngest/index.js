a03c0bef986f70cb9aab982ee7bff8622ad17baf
pradyumn8 <96361733+pradyumn8@users.noreply.github.com>
2025-11-14 12:01:29 +0530

    import fixed

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,6 +1,6 @@
 import { Inngest } from "inngest";
 import prisma from "../configs/prisma.js";
-import sendEmail from "../configs/sendEmail.js";
+import sendEmail from "../configs/nodemailer.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "my-app" });
